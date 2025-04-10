10ff07d1edba125980a1ea7f1ca172b332dc3ce2
Bilenie Tilahun <bilumekb@gmail.com>
2025-04-10 11:26:14 -0500

    first Java project

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println( "shoes");
+        System.out.println( "bags");
+        System.out.println( "sunglasses");
+        System.out.println( "clothes");
+        System.out.println( "jeans");
+        System.out.println( "heels");
+        System.out.println( "lipgloss");
+        System.out.println( "candy");
+        System.out.println( "sneakers");
+        System.out.println( "makeups");
+    }
+}
