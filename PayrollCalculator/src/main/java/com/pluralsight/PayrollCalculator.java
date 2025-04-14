155dee76e2cd3cf808e6afd66e3138db56b616b9
Bilenie Tilahun <bilumekb@gmail.com>
2025-04-14 15:02:23 -0500

    Completed Mod3 - Exercise 4 - Payroll Calculator

---
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+//print the first question for the users.
+        System.out.println("What is your name?");
+//get the name from the users and store it in variable name.
+        String name = myScanner.nextLine();
+//print a message for the users.
+        System.out.println("What a beautiful name!!!");
+//print the second question for the users.
+        System.out.println("What is your hours worked this week?");
+
+//get the hour worked and store it in hourlyWorked.
+        float hourlyWorked = myScanner.nextFloat();
+
+//print the second question for the users.
+        System.out.println("What is your pay rate?");
+
+//get the amount paid from the users and store it in payRate.
+        float payRate = myScanner.nextFloat();
+
+        float totalGrossPay = grossPay(hourlyWorked, payRate);
+
+        System.out.println("The gross pay for"+ " " + name + ","+ " "+ "is"+ " " +"$" + totalGrossPay);
+        System.out.printf("The gross pay for %s, is $%.2f",name, totalGrossPay);
+    }
+
+    public static float grossPay(float var1, float var2){
+
+        return var1 * var2;
+    }
+
+}
+
+
+
+
