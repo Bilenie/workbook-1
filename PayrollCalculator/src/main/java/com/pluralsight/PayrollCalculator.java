440ea0a7f92530bd6aa49dfc02a8f22a38b322ba
Bilenie Tilahun <bilumekb@gmail.com>
2025-04-14 20:09:56 -0500

    Completed Mod3 - Exercise 5 - Basic Calculator

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -8,6 +8,10 @@ public class PayrollCalculator {
         Scanner myScanner = new Scanner(System.in);
 
 //print the first question for the users.
+        askUser(myScanner);
+    }
+
+    public static void askUser(Scanner myScanner) {
         System.out.println("What is your name?");
 //get the name from the users and store it in variable name.
         String name = myScanner.nextLine();
@@ -27,11 +31,20 @@ public class PayrollCalculator {
 
         float totalGrossPay = grossPay(hourlyWorked, payRate);
 
-        System.out.println("The gross pay for"+ " " + name + ","+ " "+ "is"+ " " +"$" + totalGrossPay);
-        System.out.printf("The gross pay for %s, is $%.2f",name, totalGrossPay);
+        if (hourlyWorked > 40) {
+            double overTimeHour = hourlyWorked - 40;
+            double overTime = overTimeHour * (payRate * 1.5);
+            System.out.println("your over time is " + overTimeHour + " " + "your over time pay is $" + " " + overTime);
+        } else {
+            System.out.println("No over time");
+        }
+
+
+        System.out.println("The gross pay for" + " " + name + "," + " " + "is" + " " + "$" + totalGrossPay);
+        System.out.printf("The gross pay for %s, is $%.2f", name, totalGrossPay);
     }
 
-    public static float grossPay(float var1, float var2){
+    public static float grossPay(float var1, float var2) {
 
         return var1 * var2;
     }
